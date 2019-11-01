f01de740c110941b76af03007b22e93e738e7bcc
edgarrambo <ragde956@gmail.com>
2019-11-01 05:05:11 +0000

    initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,13 +7,17 @@ before_action :authenticate_user!, only: [:new, :create]
 
 
 	def new
-		@place =Place.new
+		@place = Place.new
 	end
 
 def create
 	current_user.places.create(place_params)
 	redirect_to root_path
 end
+
+def show
+	@place =Place.find(params[:id])
+	end
 
 private
 
