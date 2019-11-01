4d6872e9d596931c42b136126652d221055aae03
edgarrambo <ragde956@gmail.com>
2019-11-01 20:33:37 +0000

    intial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit]
+before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 
 def index
@@ -33,6 +33,11 @@ def update
 	@place =Place.find(params[:id])
 	@place.update_attributes(place_params)
 	redirect_to root_path
+
+	if @place.user != current_user
+		return render plain: 'NOT ALLOWED' , status: :forbidden
+	end
+	
 	end
 	
 def destroy
