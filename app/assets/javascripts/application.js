3dcc0e0526cd21081f6261bd74ede14876c69393
edgarrambo <ragde956@gmail.com>
2019-11-04 21:03:56 +0000

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= request jquery
-//= request jquery_ujs
+//= require rails-ujs
 //= require activestorage
-
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
