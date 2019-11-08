01af34a528308e3fa2928824a87265b6b452e8df
edgarrambo <ragde956@gmail.com>
2019-11-08 02:38:55 +0000

    email setup

--- app/mailers/notificationmailer_mailer.rb
+++ app/mailers/notificationmailer_mailer.rb
@@ -3,7 +3,7 @@ class NotificationmailerMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "ragde956@gmail.com",
+    mail(to: "edgar.alonzo.arellano.1990@gmail.com",
          subject: "A comment has been added to your place")
   end
 end
